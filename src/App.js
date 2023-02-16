946d6ad2158f0336899027cb147c99f60335ba43
clayton-klein <csklein87@hotmail.com>
2023-02-16 15:51:52 -0300

    feat: add roll button and refactor Die component to render dynamically.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import "./App.css";
-import Die from "../src/components/Die.js";
+import Die from "./components/Die.js";
+import { useState } from "react";
 
 export default function App() {
+  const [diceNumbers, setDiceNumbers] = useState(allNewDice());
+
+  const diceElements = diceNumbers.map((number) => {
+    return <Die key={Math.random()} value={number} />;
+  });
+
+  function allNewDice() {
+    const numbersArray = [];
+
+    for (let i = 0; i < 10; i++) {
+      let randomNumber = Math.floor(Math.random() * 6 + 1);
+      numbersArray.push(randomNumber);
+    }
+
+    return numbersArray;
+  }
+
+  function rollDice() {
+    setDiceNumbers(allNewDice())
+  }
+
   return (
     <div className="App">
       <main className="gameContainer">
-        <div className="diceContainer">
-          <Die value="1" />
-          <Die value="2" />
-          <Die value="3" />
-          <Die value="4" />
-          <Die value="5" />
-          <Die value="6" />
-          <Die value="5" />
-          <Die value="4" />
-          <Die value="3" />
-          <Die value="2" />
-        </div>
+        <div className="diceContainer">{diceElements}</div>
+        <button className="rollBtn" onClick={rollDice}>Rolar</button>
       </main>
     </div>
   );
