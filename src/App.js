3c03dee3a82a5f87da0755cf453658c78beeb745
clayton-klein <csklein87@hotmail.com>
2023-02-16 20:07:02 -0300

    refacyor: change die bg color dynamically when clicked.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ export default function App() {
   const [diceNumbers, setDiceNumbers] = useState(allNewDice());
 
   const diceElements = diceNumbers.map((number) => {
-    return <Die key={Math.random()} value={number} />;
+    return (
+      <Die
+        key={number.id}
+        value={number.value}
+        isHeld={number.isHeld}
+        // to pass an argument to the callback func in this case
+        // I had to put it inside another 'hollow' callback function
+        funcHoldDice={() => holdDice(number.id)}
+        id={number.id}
+      />
+    );
   });
 
   function allNewDice() {
@@ -14,21 +24,35 @@ export default function App() {
 
     for (let i = 0; i < 10; i++) {
       let randomNumber = Math.floor(Math.random() * 6 + 1);
-      numbersArray.push(randomNumber);
+
+      numbersArray.push({
+        id: i,
+        value: randomNumber,
+        isHeld: false,
+      });
     }
 
     return numbersArray;
   }
 
   function rollDice() {
-    setDiceNumbers(allNewDice())
+    setDiceNumbers(allNewDice());
+  }
+
+  function holdDice(id) {
+    setDiceNumbers(prevArray => prevArray.map(die => {
+      return die.id === id ? {...die, isHeld: !die.isHeld} : die;
+    })
+    )
   }
 
   return (
     <div className="App">
       <main className="gameContainer">
         <div className="diceContainer">{diceElements}</div>
-        <button className="rollBtn" onClick={rollDice}>Rolar</button>
+        <button className="rollBtn" onClick={rollDice}>
+          Rolar
+        </button>
       </main>
     </div>
   );
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,5 +1,23 @@
 import "../styles/Die.css";
 
 export default function Die(props) {
-  return <div className="dieFace">{props.value}</div>;
+  // dynamic styles
+  const styles = {
+    // use camel-case on the properties names
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+
+  return (
+    // in this case the name of the attribute MUST be 'style'
+    // and it should hold as it's value the variable (styles)
+    // containing the object with the CSS properties/styles.
+    <div
+      className="dieFace"
+      style={styles}
+      onClick={props.funcHoldDice}
+      id={props.id}
+    >
+      {props.value}
+    </div>
+  );
 }
