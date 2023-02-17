6fa6e50e452c36244efd65509a0fdb91abc7b98c
clayton-klein <csklein87@hotmail.com>
2023-02-17 17:01:46 -0300

    feat: add confetti and reset func after the game is over.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Die from "./components/Die.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
 
 export default function App() {
   const [diceNumbers, setDiceNumbers] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
 
   const diceElements = diceNumbers.map((number) => {
     return (
@@ -36,23 +38,50 @@ export default function App() {
   }
 
   function rollDice() {
-    setDiceNumbers(allNewDice());
+    if (tenzies) {
+      setTenzies(false);
+      setDiceNumbers(allNewDice());
+    }
+
+    setDiceNumbers((prevDice) =>
+      prevDice.map((die) => {
+        return die.isHeld
+          ? die
+          : { ...die, value: Math.floor(Math.random() * 6 + 1) };
+      })
+    );
   }
 
   function holdDice(id) {
-    setDiceNumbers(prevArray => prevArray.map(die => {
-      return die.id === id ? {...die, isHeld: !die.isHeld} : die;
-    })
-    )
+    setDiceNumbers((prevArray) =>
+      prevArray.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
   }
+
+  useEffect(() => {
+    if (
+      diceNumbers.every((dice) => dice.isHeld === true) &&
+      diceNumbers.every((dice) => dice.value === diceNumbers[0].value)
+    ) {
+      setTenzies(true);
+    }
+  }, [tenzies, diceNumbers]);
 
   return (
     <div className="App">
       <main className="gameContainer">
+        <h1 className="gameContainer__title">Tenzies</h1>
+        <p className="gameContainer__instructions">
+          Role até que todos os dados sejam iguais. Click em cada dado para
+          "congelá-lo" no valor atual entre as rolagens.
+        </p>
         <div className="diceContainer">{diceElements}</div>
         <button className="rollBtn" onClick={rollDice}>
-          Rolar
+          {tenzies ? "Novo Jogo" : "Rolar"}
         </button>
+        {tenzies && <Confetti />}
       </main>
     </div>
   );
